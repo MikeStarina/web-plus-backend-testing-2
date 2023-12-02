eb6cd8c48081d31edc8ebfeb93a39fa50dfd6d5d
Mike Starina <x@pnhd.ru>
2023-12-02 20:26:00 +0300

    tests added

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,4 +1,4 @@
-import { PostsService } from './posts.service';
+import { Post, PostsService } from './posts.service';
 
 describe('PostsService', () => {
   let postsService: PostsService;
@@ -21,11 +21,50 @@ describe('PostsService', () => {
 
     it('should return all posts if called without options', () => {
       // реализуйте тест-кейс
+      const result = postsService.findMany()
+
+      expect(result.length).toEqual(posts.length);
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const skipOption = {
+        skip: 2
+      }
+
+      const skipResult = postsService.findMany(skipOption);
+
+      expect(skipResult.length).toEqual(posts.length - skipOption.skip);
+
+
+      const limitOption = {
+        limit: 2
+      }
+
+      const limitResult = postsService.findMany(limitOption);
+
+      expect(limitResult.length).toEqual(limitOption.limit);
     });
+
+    it('should delete post', () => {
+
+      const id = 1;
+      postsService.delete(id.toString());
+      const result = postsService.findMany();
+
+      expect(result.length).toEqual(posts.length - id);
+    })
+
+    it('should update post', () => {
+      const postId = 1;
+      const post = {
+        text: 'new text'
+      };
+
+      postsService.update(postId.toString(), post);
+      const result = postsService.find(postId.toString());
+      console.log(result);
+      expect(result).toHaveProperty('text', post.text);
+    })
 
     // реализуйте недостающие тест-кейсы
   });
